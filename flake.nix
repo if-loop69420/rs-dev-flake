2536b031fbd9a7f8453f2be7a5a7b1e26584568a
void toni <81078181+if-loop69420@users.noreply.github.com>
2023-07-18 14:07:10 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,15 +2,20 @@
   description = "A very basic flake";
 
   inputs = {
-    flake-utils.url = github:numtide/flake-utils;
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+
   };
 
-  outputs = { self, nixpkgs, flake-utils}:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in 
-        {
-          devShells.default = import ./shell.nix { inherit pkgs; };
-        }
-      );
+  outputs = {self, nixpkgs, rust-overlay,}:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [rust-overlay.overlays.default];
+    };
+    toolchain = pkgs.rust-bin.fromRustupToolchainFile ./toolchain.toml;
+  in {
+    devShells.${system}.default = pkgs.callPackage ./shell.nix { inherit pkgs; toolchain = toolchain; };
+  };
 }
